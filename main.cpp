89360abff6adb31e370a856b96eada5e9cbd1adc
shemuelong <31332054+shemuelong@users.noreply.github.com>
2018-02-05 16:35:49 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,46 +1,12 @@
 #include <iostream>
 
-void selectionSort(std::string &a){
-    //This function will sort an array, using ASCII values.
-    //In this assignment, all the spaces of an anagram will go to the front, with the characters in the back.
+void selectionSort(std::string &a);
+//This function will sort an array, using ASCII values.
+//In this assignment, all the spaces of an anagram will go to the front, with the characters in the back.
 
-    bool swapped;   //swapped for sorting method
-    do{
-        swapped = false; //bool is false
-        for(int i = 0; i <= a.length();  i++)
-            //For loop iterattes over length of a, swapping one character with another until
-            //there is an iteration without doing one swap
-        {
-            if ((a[i] > a[i+1]) && (i+1 < a.length()))
-                //Swap will occur if the intended character is greater alphabetically than the subsequent one.
-                //Second comparison in if statement prevents comparing the last letter of the string with a memory address out of bounds
-            {
-                char temp = a[i]; //temp variable to store the character that will be swapped
-                a[i] = a[i+1];    //current subsequent index is copied into the current index
-                a[i+1] = temp;    //subsequent index is set to temp (the previous value)
-                swapped = true;   //boolean for the final check
-            }
-        }
-    }while(swapped);
-}
-
-
-void deleteSpaces(std::string &a){
-    //Given that the string is already sorted, with spaces in the front and characters in the back,
-    //this function will find the first instance of a character, and delete everything before it.
-
-    int index = 0;  //Temporary variable to store index of first character
-    for(int i = 0; i < a.length(); i++){ //For loop will iterate through length of string
-        if (a[i] >= 'A') {
-            //When the first character is found, we will store the index and break from the loop
-            index = i;
-            break;
-        }
-    }
-    a.erase(0,index);
-    //Everything from the beginning to the first index will then be deleted.
-}
-
+void deleteSpaces(std::string &a);
+//Given that the string is already sorted, with spaces in the front and characters in the back,
+//this function will find the first instance of a character, and delete everything before it.
 
 int main() {
 
@@ -66,3 +32,44 @@ int main() {
     else            //If the two fail the comparison, they are not anagrams
        std::cout << "The two are not an anagram pair" << std::endl;
 }
+
+void selectionSort(std::string &a){
+    //This function will sort an array, using ASCII values.
+    //In this assignment, all the spaces of an anagram will go to the front, with the characters in the back.
+
+    bool swapped;   //swapped for sorting method
+    do{
+        swapped = false; //bool is false
+        for(int i = 0; i <= a.length();  i++)
+            //For loop iterattes over length of a, swapping one character with another until
+            //there is an iteration without doing one swap
+        {
+            if ((a[i] > a[i+1]) && (i+1 < a.length()))
+                //Swap will occur if the intended character is greater alphabetically than the subsequent one.
+                //Second comparison in if statement prevents comparing the last letter of the string with a memory address out of bounds
+            {
+                char temp = a[i]; //temp variable to store the character that will be swapped
+                a[i] = a[i+1];    //current subsequent index is copied into the current index
+                a[i+1] = temp;    //subsequent index is set to temp (the previous value)
+                swapped = true;   //boolean for the final check
+            }
+        }
+    }while(swapped);
+}
+
+void deleteSpaces(std::string &a){
+    //Given that the string is already sorted, with spaces in the front and characters in the back,
+    //this function will find the first instance of a character, and delete everything before it.
+
+    int index = 0;  //Temporary variable to store index of first character
+    for(int i = 0; i < a.length(); i++){ //For loop will iterate through length of string
+        if (a[i] >= 'A') {
+            //When the first character is found, we will store the index and break from the loop
+            index = i;
+            break;
+        }
+    }
+    a.erase(0,index);
+    //Everything from the beginning to the first index will then be deleted.
+}
+
